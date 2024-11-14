cdeb015d99df85c91e046d3250fe5716fbe8c5eb
SangWon7242 <gotkdqja@gmail.com>
2024-11-14 20:52:23 +0900

    usr/article/detail 명령 처리, 가장 마지막 게시물이 상세보기 되도록

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int lastArticleId = 0; // 전역변수
+    int lastArticleId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 자바 텍스트 게시판 시작 ==");
 
@@ -13,7 +14,7 @@ public class Main {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("/usr/article/write")) {
+      if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
         String subject = sc.nextLine();
@@ -24,15 +25,26 @@ public class Main {
         int id = ++lastArticleId;
 
         Article article = new Article(id, subject, content);
+        lastArticle = article;
 
         System.out.println("생성 된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
-      else if(cmd.equals("exit")) {
+      else if (cmd.equals("/usr/article/detail")) {
+        Article article = lastArticle;
+        if(article == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.subject);
+        System.out.printf("내용 : %s\n", article.content);
+      } else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
-      }
-      else {
+      } else {
         System.out.println("잘못 된 명령어입니다.");
       }
     }
