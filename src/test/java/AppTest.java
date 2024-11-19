d5e0ef3e790bd4bb7f3d71af28b90f02e415b807
SangWon7242 <gotkdqja@gmail.com>
2024-11-19 19:10:09 +0900

    을 기준으로 파라미터를 세밀하게 쪼개기

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,8 +8,12 @@ public class AppTest {
     String[] queryStringBits = queryString.split("&");
     System.out.println(Arrays.toString(queryStringBits));
 
-    // for(String bit : queryStringBits) System.out.println(bit);
+    for(String bit : queryStringBits) {
+      String[] bitBits = bit.split("=");
+      String paramName = bitBits[0];
+      String paramValue = bitBits[1];
 
-    Arrays.stream(queryStringBits).forEach(System.out::println);
+      System.out.printf("%s : %s\n", paramName, paramValue);
+    }
   }
 }
